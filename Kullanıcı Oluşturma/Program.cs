e1c9629ab6defe567d1310a83c60dec6f260a479
Arda Dere <ardadere114@gmail.com>
2025-03-07 20:19:06 +0300

    Proje dosyası ekle.

---
+++ Kullanıcı Oluşturma/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Kullanıcı_Oluşturma;
+using Kullanıcı_Oluşturma;
+
+class Program
+{
+    static void Main()
+    {
+        User kullanici = new User();
+        kullanici.SetAdSoyad("Arda Dere");
+        kullanici.SetYas(19);
+        kullanici.Email = "ardadere114@gmail.com";
+
+        kullanici.BilgileriGoster();
+    }
+}
---
+++ Kullanıcı Oluşturma/User.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kullanıcı_Oluşturma
+{
+    class User
+    {
+        private string adSoyad;
+        private int yas;
+        public string Email { get; set; }
+
+        
+        public string GetAdSoyad()
+        {
+            return adSoyad;
+        }
+
+        public void SetAdSoyad(string deger)
+        {
+            adSoyad = deger;
+        }
+
+        public int GetYas()
+        {
+            return yas;
+        }
+
+        public void SetYas(int deger)
+        {
+            if (deger > 0) 
+                yas = deger;
+            else
+                Console.WriteLine("Geçerli bir yaş giriniz!");
+        }
+
+        
+        public void BilgileriGoster()
+        {
+            Console.WriteLine($"Ad Soyad: {adSoyad}");
+            Console.WriteLine($"Yaş: {yas}");
+            Console.WriteLine($"E-Posta: {Email}");
+        }
+    }
+}
